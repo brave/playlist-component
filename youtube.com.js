5434730bf7342f1ba5c057f1640882bb38604a85
sangwoo.ko <sangwoo108@gmail.com>
2023-01-30 08:57:04 +0900

    Address code review

--- youtube.com.js
+++ youtube.com.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 The Brave Authors. All rights reserved.
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -8,7 +8,7 @@ const siteSpecificDetector = { // eslint-disable-line no-unused-vars
   getThumbnail: function () {
     let thumbnail = window.ytplayer?.bootstrapPlayerResponse?.videoDetails?.thumbnail?.thumbnails
     if (thumbnail && Array.isArray(thumbnail)) {
-      // The last one is known to be one with the hight resolution
+      // The last one is known to be one with the highest resolution
       thumbnail = thumbnail[thumbnail.length - 1]?.url
     }
     return (typeof(thumbnail) === 'string' && (thumbnail.startsWith('/') || thumbnail.startsWith('http://') || thumbnail.startsWith('https://'))) 
