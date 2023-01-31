d7d601d7bbb5fa59e1fb4fbf8bd65750bce0700c
sangwoo.ko <sangwoo108@gmail.com>
2023-02-01 08:43:28 +0900

    Address code review

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,25 +30,6 @@ module.exports = {
           'License, v. 2.0. If a copy of the MPL was not distributed with this file,',
           'You can obtain one at https://mozilla.org/MPL/2.0/.'
         ],
-        'altHeaders': [
-          [
-            'Copyright (c) {YEAR} The Brave Authors. All rights reserved.',
-            '* This Source Code Form is subject to the terms of the Mozilla Public',
-            '* License, v. 2.0. If a copy of the MPL was not distributed with this file,',
-            '* You can obtain one at https://mozilla.org/MPL/2.0/.'
-          ],
-          [
-            'This Source Code Form is subject to the terms of the Mozilla Public',
-            '* License, v. 2.0. If a copy of the MPL was not distributed with this file,',
-            '* You can obtain one at https://mozilla.org/MPL/2.0/.'
-          ],
-          [
-            'Copyright (c) {YEAR} The Brave Authors. All rights reserved.',
-            'This Source Code Form is subject to the terms of the Mozilla Public',
-            'License, v. 2.0. If a copy of the MPL was not distributed with this file,',
-            'you can obtain one at https://mozilla.org/MPL/2.0/.'
-          ]
-        ]
       }
     ],
     'quote-props': [
